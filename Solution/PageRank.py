f61280c30a855c77481053d2ced0f0a01333be02
Zjrua <zhangjuruo@gmail.com>
2021-02-07 11:57:59 +0800

    Process the dead end

--- Solution/PageRank.py
+++ Solution/PageRank.py
@@ -1,7 +1,8 @@
 import csv
 import numpy as np
+import pandas as pd
 
-csvFile = open("E:\\StudyAndWork\\MCM2021\\Problem\\2021_ICM_Problem_D_Data\\influence_data.csv", "r", encoding='UTF-8')
+csvFile = open("influence_data.csv", "r", encoding='UTF-8')
 reader = csv.reader(csvFile)
 g = np.zeros((5604, 5604), dtype = np.int)
 g1 = np.zeros((5604, 5604), dtype = np.float)
@@ -15,30 +16,25 @@ def create_matrix():
                 id_list.append(item[0])
             if item[4] not in id_list:
                 id_list.append(item[4])
-            g[id_list.index(item[0])][id_list.index(item[4])] = 1
-
-create_matrix()
-#print(id_list)
-#print(g)
+            g[id_list.index(item[4])][id_list.index(item[0])] = 1
 #exit()
+create_matrix()
+
 for i in range(0, 5604):
     sum = 0
     for j in range(0, 5604):
         sum = sum + g[i][j]
     if sum == 0:
+        #print(i)
         continue
     for j in range(0, 5604):
-        g1[i][j] = g[i][j] * 1 / sum
+        g1[i][j] = g[i][j] / sum
 #print(g1)
-
-v = np.zeros((5604, 1), dtype = np.float)
-op = {}
-for i in range(0, 5604):
-    v[i] = (1 / 5603)
-#print(v)
+v = [1/5603 for x in range(0, 5604)]
 for times in range(1, 1000):
-    v = np.dot(g1, v)
-#print(v)
+    v = np.dot(v, g1)
+
+print(v)
 
 with open("output.csv", "w", newline="") as csvFile:
     reader = csv.writer(csvFile, dialect="excel")
